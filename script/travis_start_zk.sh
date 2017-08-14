33ed77fd651d3650c2ce8721ed0956a90bdd521a
Matthieu Nottale <matthieu.nottale@docker.com>
2017-08-14 10:31:49 +0200

    travis: strace zk to figure out why it doesn't start.

--- script/travis_start_zk.sh
+++ script/travis_start_zk.sh
@@ -1,10 +1,16 @@
 #! /bin/bash
 
+if ! test -e /usr/bin/strace; then
+  sudo apt-get install strace
+fi
+
 while true; do
-  ./zk/bin/zkServer.sh start ./zk/conf/zoo.cfg
+  strace -f -o /tmp/zk.trace ./zk/bin/zkServer.sh start ./zk/conf/zoo.cfg &
+  sleep 2
   if echo stat |nc localhost 2181 |grep -q Mode; then
     break
   fi
   echo zk did not start properly, retrying...
+  cat /tmp/zk.trace
   ./zk/bin/zkServer.sh stop
 done
